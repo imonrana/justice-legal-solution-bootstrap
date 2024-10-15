fa98da01d090006374265baa3ecafd2d2ac8abe8
imonrana <info.imon@yahoo.com>
2024-10-16 02:02:07 +0600

    complete webpage

---
+++ js/sript.js
@@ -0,0 +1,13 @@
+// slik slider start
+$('.item').slick({
+
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow:"<button type='button' <i class='fa fa-angle-left prev' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' <i class='fa fa-angle-right next' aria-hidden='true'></i></button>",
+  });
+
+// slik slider end
